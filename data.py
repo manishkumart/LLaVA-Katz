398edee65f0f4b064bce0f20fddf070d34e47157
Manish kumar thota <manish.thota1999@gmail.com>
2023-11-02 14:06:09 -0400

    Updated Repo

---
+++ data.py
@@ -0,0 +1,20 @@
+Target:
+
+[{"id": "GCC_train_002582585", 
+ "image": "GCC_train_002582585.jpg", 
+ "caption": " ", 
+ "path": "Images"
+ 
+ },{}]
+
+
+Original:
+
+{    "name": "week_01_page_001.png",
+    "path": ".\\data\\imgs\\week_01\\week_01_page_001.png",
+    "week": 1,
+     "page": 1
+    },
+
+Give me a python code for such that the path will be same just change "imgs" to "Images", and convert all the Original to the targert , os the ID will the name without the image format
+and the name will the the image name with the format and leave the caption as empty string anf then path as mentioned.
